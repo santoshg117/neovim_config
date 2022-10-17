9fd5df9d40bef5e098ef16411d7b181fde42e695
santoshg117 <santosh117@pm.me>
2022-10-16 17:41:26 -0700

    update

---
+++ populate_bashrc.sh
@@ -0,0 +1,5 @@
+echo "Exported VIMCONFIG"
+echo "export VIMCONFIG=~/.config/nvim" >> ~/.bashrc
+echo "Exported VIMDATA"
+echo "export VIMDATA=~/.local/share/nvim" >> ~/.bashrc
+echo "\n" >> ~/.bashrc
