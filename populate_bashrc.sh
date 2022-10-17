82c9a9e3f266bbaa2b1a5402e66fbdf828c341a2
santoshg117 <santosh117@pm.me>
2022-10-17 07:35:19 -0700

    Changed so that we just use the correct directory

--- populate_bashrc.sh
+++ populate_bashrc.sh
@@ -3,3 +3,5 @@ echo "export VIMCONFIG=~/.config/nvim" >> ~/.bashrc
 echo "Exported VIMDATA"
 echo "export VIMDATA=~/.local/share/nvim" >> ~/.bashrc
 echo "\n" >> ~/.bashrc
+
+curl -fLo autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
